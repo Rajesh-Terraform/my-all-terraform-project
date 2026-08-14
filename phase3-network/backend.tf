c4f487e04272bdeedf5fb53b47b9f3a6d71b803f
rajeshjonnalagadda294@gmail.com <rajeshjonnalagadda294@gmail.com>
2026-08-14 19:18:37 +0530

    all terraform codes

--- phase3-network/backend.tf
+++ phase3-network/backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "= 1.9.8"
 
   backend "s3" {
-    bucket = "YOUR-HUB-TERRAFORM-STATE-BUCKET"
+    bucket = "harish-gaddam-bucket123 "
 
     key = "phase3-network/terraform.tfstate"
 
